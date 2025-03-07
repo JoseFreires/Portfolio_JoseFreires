7fea78af092e660b7c68e1107a207c36804cef30
Jose Freires <joseguifreires@gmail.com>
2025-03-07 12:46:33 -0300

    Resolving bug on Routes

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}/>
-      <Route path="/projects" element={<Projects />}/>
+      <Route path="projects" element={<Projects />}/>
     </Routes>
   </BrowserRouter>
 )
